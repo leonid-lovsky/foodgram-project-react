88a17cc599bed4b9da77d9768df4a71f692a145d
Leonid Lovsky <leonid.lovsky@yandex.ru>
2023-01-31 22:14:16 +0200

    Docker

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r@8by2u2d34c^ar5^4f@#t^fp$j#4y7n_tcj!crl2+#e-4zc4x'
+SECRET_KEY = 'django-insecure-@sqk$b&6+$w67#iaa$8a+76zf=z=xc---739#d54ws5=q=a&t='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] # TODO
 
 
 # Application definition
