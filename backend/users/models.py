6de0fe68d3723e925182f726fbdfa000b89124fe
Leonid Lovsky <leonid.lovsky@yandex.ru>
2023-02-10 15:58:14 +0200

    models

--- backend/users/models.py
+++ backend/users/models.py
@@ -10,34 +10,7 @@ class User(AbstractUser):
     email = models.EmailField(
         _('email address'),
         unique=True,
-        blank=False,
-    )
-    username = models.CharField(
-        _('username'),
-        max_length=150,
-        unique=True,
-        help_text=_(
-            'Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
-        validators=[username_validator],
-        error_messages={
-            'unique': _("A user with that username already exists."),
-        },
-        blank=False,
-    )
-    first_name = models.CharField(
-        _('first name'),
-        max_length=150,
-        blank=False,
-    )
-    last_name = models.CharField(
-        _('last name'),
-        max_length=150,
-        blank=False,
     )
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = [
-        'username',
-        'first_name',
-        'last_name',
-    ]
+    REQUIRED_FIELDS = []
