ea63515dbe8b03f6b90a2e5a7261d063a4ab603c
Leonid Lovsky <leonid.lovsky@yandex.ru>
2023-02-11 11:41:41 +0200

    static

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 # from djoser.views import UserViewSet
@@ -18,13 +20,13 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls'))
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(
-#         settings.STATIC_URL,
-#         document_root=settings.STATIC_ROOT
-#     )
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+    )
 
-#     urlpatterns += static(
-#         settings.MEDIA_URL,
-#         document_root=settings.MEDIA_ROOT
-#     )
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
