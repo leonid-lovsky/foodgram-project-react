2b813ea3aae067c7307e6fe4f0f58c6ad8ed1d26
Leonid Lovsky <leonid.lovsky@yandex.ru>
2023-02-14 18:14:47 +0200

    download_shopping_cart

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -103,3 +103,11 @@ class RecipeViewSet(ModelViewSet):
             return Response(
                 status=HTTP_204_NO_CONTENT
             )
+
+    @action(
+        detail=False,
+        url_path='download_shopping_cart',
+        permission_classes=[IsAuthenticated]
+    )
+    def download_shopping_cart(self, request, pk=None):
+        pass
