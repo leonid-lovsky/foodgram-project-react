fa8b1b4855dbd63cc3683d9e96555b162327da45
Leonid Lovsky <leonid.lovsky@yandex.ru>
2023-02-16 18:08:43 +0200

    admin

--- backend/ingredients/admin.py
+++ backend/ingredients/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+
 from .models import Ingredient
 
 
 @admin.register(Ingredient)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ['name', 'measurement_unit']
+    list_display = ['name', 'measurement_unit', 'get_usage']
     search_fields = ['name']
+
+    def get_usage(self, obj):
+        return obj.recipe_set.count()
+
+    get_usage.short_description = _('Использований')
--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -6,13 +6,31 @@ from .models import Recipe
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ['name', 'author', 'cooking_time', 'get_favorites']
+    list_display = ['name', 'author', 'cooking_time',
+                    'get_ingredients', 'get_favorites', 'get_shopping_carts']
     search_fields = ['author', 'name']
+    # TODO: list filter name
     list_filter = ['tags']
+    # TODO: inline models
     filter_horizontal = ['tags']
-    # filter_horizontal = ['ingredients', 'tags']
+    # filter_horizontal = ['ingredients']
+    # inlines = ['TagInline']
+    # inlines = ['IngredientInline']
+
+    def get_ingredients(self, obj):
+        return obj.recipeingredient_set.count()
+
+    get_ingredients.short_description = _('Ингредиентов')
+
 
     def get_favorites(self, obj):
         return obj.recipefavorite_set.count()
 
-    get_favorites.short_description = _('Популярнсть')
+    get_favorites.short_description = _('В избранном')
+
+    def get_shopping_carts(self, obj):
+        return obj.recipeshoppingcart_set.count()
+
+    get_shopping_carts.short_description = _('В корзине')
+
+
