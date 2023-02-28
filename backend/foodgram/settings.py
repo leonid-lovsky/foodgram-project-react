e17ad9487713b718c8bfcde188b1b8fcacb91a47
Leonid Lovsky <leonid.lovsky@yandex.ru>
2023-02-28 20:47:05 +0200

    commit

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -9,9 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG') == 'TRUE'
+DEBUG = os.getenv('DEBUG', 'TRUE') == 'TRUE'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split()
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -143,3 +143,17 @@ CORS_URLS_REGEX = r'^/api/.*$'
 # CORS_ALLOW_CREDENTIALS = True
 
 # LOGIN_REDIRECT_URL = '/accounts/profile/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
