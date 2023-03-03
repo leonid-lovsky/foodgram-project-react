fc7c4646c622783f429174ce355ed03d1a6212c9
Leonid Lovsky <leonid.lovsky@yandex.ru>
2023-03-03 15:36:51 +0200

    commit

--- backend/recipes/management/commands/load_data.py
+++ backend/recipes/management/commands/load_data.py
@@ -2,7 +2,8 @@ import json
 
 from django.core.management.base import BaseCommand
 from django.utils.translation import gettext_lazy as _
-from recipes.models import *
+
+from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
@@ -10,7 +11,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.WARNING(_('Загрузка ингредиентов')))
-        with open('../data/ingredients.json', encoding='utf-8') as file:
+        with open('data/ingredients.json', encoding='utf-8') as file:
             ingredients = json.loads(file.read())
             for ingredient in ingredients:
                 Ingredient.objects.get_or_create(**ingredient)
